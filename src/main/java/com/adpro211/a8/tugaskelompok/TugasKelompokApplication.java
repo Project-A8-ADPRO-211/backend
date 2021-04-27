0da0e554b1e90b9f8544fe1758352147190d5590
Naradhipa Bhary <mydhipa@gmail.com>
2021-04-27 09:59:17 +0700

    inital commit

---
+++ src/main/java/com/adpro211/a8/tugaskelompok/TugasKelompokApplication.java
@@ -0,0 +1,13 @@
+package com.adpro211.a8.tugaskelompok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasKelompokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TugasKelompokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adpro211/a8/tugaskelompok/TugasKelompokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adpro211.a8.tugaskelompok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasKelompokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
