fd5426594390e7ecf3096f2057c38a86725578ae
advistasyam <advis.tasyah@ui.ac.id>
2021-05-02 16:34:02 +0700

    add simple test for create product

---
+++ src/test/java/com/adpro211/a8/tugaskelompok/product/controller/ProductControllerTest.java
@@ -0,0 +1,96 @@
+package com.adpro211.a8.tugaskelompok.product.controller;
+
+import com.adpro211.a8.tugaskelompok.auths.controller.SignupController;
+import com.adpro211.a8.tugaskelompok.auths.models.account.Account;
+import com.adpro211.a8.tugaskelompok.auths.models.account.Seller;
+import com.adpro211.a8.tugaskelompok.auths.repository.AccountRepository;
+import com.adpro211.a8.tugaskelompok.auths.repository.AdminRepository;
+import com.adpro211.a8.tugaskelompok.auths.repository.BuyerRepository;
+import com.adpro211.a8.tugaskelompok.auths.repository.SellerRepository;
+import com.adpro211.a8.tugaskelompok.auths.service.AccountService;
+import com.adpro211.a8.tugaskelompok.auths.service.AuthService;
+import com.adpro211.a8.tugaskelompok.auths.service.JWTService;
+import com.adpro211.a8.tugaskelompok.product.model.Product;
+import com.adpro211.a8.tugaskelompok.product.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
+    private AuthService authService;
+
+    @MockBean
+    private JWTService jwtService;
+
+    @MockBean
+    private AccountRepository accountRepository;
+
+    @MockBean
+    private AdminRepository adminRepository;
+
+    @MockBean
+    private BuyerRepository buyerRepository;
+
+    @MockBean
+    private SellerRepository sellerRepository;
+
+    private Account account;
+    private Product product;
+    private Iterable<Product> productIterable;
+
+    private String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    @BeforeEach
+    void setUp() {
+        account = new Seller();
+        account.setName("advis");
+        account.setId(1);
+        account.setEmail("advistasyam@gmail.com");
+
+        product = new Product();
+        product.setId(2);
+        product.setName("bakso");
+        product.setPrice(1000);
+        product.setStock(50);
+        product.setDescription("makanan enak");
+        product.setImageUrl("https://google.com");
+        product.setOwnerAccount(account);
+    }
+
+    @Test
+    void testControllerGetProductSuccess() throws Exception {
+        when(productService.getProductById(2)).thenReturn(product);
+
+        mvc.perform(post("/product/create/1").contentType(MediaType.APPLICATION_JSON_VALUE).content(
+                mapToJson(new Product()))).andExpect(status().is2xxSuccessful());
+    }
+
+
+
+}
