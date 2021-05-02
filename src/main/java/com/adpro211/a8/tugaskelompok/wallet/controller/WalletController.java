27e878f9e877ff1417363666f1cdb8a7fb33fda1
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-02 17:31:24 +0700

    Implement create wallet request

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/controller/WalletController.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/controller/WalletController.java
@@ -1,5 +1,6 @@
 package com.adpro211.a8.tugaskelompok.wallet.controller;
 
+import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
 import com.adpro211.a8.tugaskelompok.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/wallet")
 public class WalletController {
+
     @Autowired
     WalletService walletService;
 
-    @PostMapping(produces = {"application/json"})
+    @PostMapping(path = "/account/{idAccount}", produces = {"application/json"})
     @ResponseBody
-    public ResponseEntity postWallet() {
-        return ResponseEntity.ok(walletService.createWallet());
+    public ResponseEntity postWallet(@PathVariable(value = "idAccount") int idAccount) {
+        return ResponseEntity.ok(walletService.createWallet(idAccount));
     }
 }
