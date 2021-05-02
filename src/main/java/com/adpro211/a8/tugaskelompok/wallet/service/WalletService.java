c86d7202abaa39a3c3c7f866f8f218585820e94a
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-02 17:29:53 +0700

    Create wallet service interface

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/service/WalletService.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/service/WalletService.java
@@ -3,6 +3,6 @@ package com.adpro211.a8.tugaskelompok.wallet.service;
 import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
 
 public interface WalletService {
-    Wallet createWallet();
+    Wallet createWallet(int id);
     Wallet getWalletById(int id);
 }
