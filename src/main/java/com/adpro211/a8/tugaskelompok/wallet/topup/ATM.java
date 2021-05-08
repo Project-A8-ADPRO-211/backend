2aea35f2e509e5ef2bbc49ab5e3f3fffa6c64fa6
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-09 02:13:46 +0700

    Create strategy ATM

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/topup/ATM.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/topup/ATM.java
@@ -1,8 +1,24 @@
 package com.adpro211.a8.tugaskelompok.wallet.topup;
 
-public class ATM implements ITopUp {
-    @Override
-    public void topup() {
+import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
 
+import java.util.Map;
+
+public class ATM implements Topup {
+
+    @Override
+    public String getType() {
+        return "ATM";
+    }
+
+    @Override
+    public void topup(Wallet wallet, Map<String, Object> requestBody) {
+        Number amountObj = (Number) requestBody.get("amount");
+        double amount = amountObj.doubleValue();
+        String noRekening = requestBody.get("noRekening").toString();
+
+        double balance = wallet.getBalance();
+        balance += amount;
+        wallet.setBalance(balance);
     }
 }
