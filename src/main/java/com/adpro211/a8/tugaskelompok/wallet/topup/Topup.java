495caa4a43fb8097e90705553b0ce4040bac66db
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-09 02:13:13 +0700

    Add topup interface

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/topup/Topup.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/topup/Topup.java
@@ -1,2 +1,9 @@
-package com.adpro211.a8.tugaskelompok.wallet.topup;public interface Topup {
+package com.adpro211.a8.tugaskelompok.wallet.topup;
+
+import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
+
+import java.util.Map;
+
+public interface Topup extends Strategy{
+    void topup(Wallet wallet, Map<String, Object> requestBody);
 }
