1a541c1c7eac4800c27935b9e77bbfbf256fc61b
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-09 02:16:06 +0700

    Add request for topup

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/controller/WalletController.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/controller/WalletController.java
@@ -3,8 +3,12 @@ package com.adpro211.a8.tugaskelompok.wallet.controller;
 import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
 import com.adpro211.a8.tugaskelompok.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/wallet")
@@ -17,5 +21,18 @@ public class WalletController {
     @ResponseBody
     public ResponseEntity postWallet(@PathVariable(value = "idAccount") int idAccount) {
         return ResponseEntity.ok(walletService.createWallet(idAccount));
+    }
+
+    @PostMapping(path = "/topup", produces = {"applicaiton/json"})
+    @ResponseBody
+    public ResponseEntity topupWallet(@RequestParam String strategy, @RequestBody Map<String, Object> request) {
+        if (!request.containsKey("idWallet")) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        Number objIdWallet = (Number) request.get("idWallet");
+        int idWallet = objIdWallet.intValue();
+        Wallet wallet = walletService.getWalletById(idWallet);
+
+        if (wallet == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return ResponseEntity.ok(walletService.topupWallet(wallet, strategy, request));
     }
 }
