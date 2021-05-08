4de17f0d33a58f1ac62c5996ba3047d0343a8cbf
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-09 02:15:11 +0700

    Change parameter for topup

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/service/WalletService.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/service/WalletService.java
@@ -2,8 +2,10 @@ package com.adpro211.a8.tugaskelompok.wallet.service;
 
 import com.adpro211.a8.tugaskelompok.wallet.models.Wallet;
 
+import java.util.Map;
+
 public interface WalletService {
     Wallet createWallet(int id);
-    Wallet topupWallet();
+    Wallet topupWallet(Wallet wallet, String type, Map<String, Object> requestBody);
     Wallet getWalletById(int id);
 }
