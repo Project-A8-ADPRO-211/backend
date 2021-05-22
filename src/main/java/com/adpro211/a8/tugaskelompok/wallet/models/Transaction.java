e51c5ad5f496997550a38ea4be24443adb4807d4
Muhamad Adamy Rayeuk <adamy.rayeuk@gmail.com>
2021-05-23 01:54:25 +0700

    Create transaction model

--- src/main/java/com/adpro211/a8/tugaskelompok/wallet/models/Transaction.java
+++ src/main/java/com/adpro211/a8/tugaskelompok/wallet/models/Transaction.java
@@ -1,2 +1,34 @@
-package com.adpro211.a8.tugaskelompok.wallet.models;public class Transaction {
+package com.adpro211.a8.tugaskelompok.wallet.models;
+
+import com.adpro211.a8.tugaskelompok.auths.models.account.Account;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "transaction")
+@Data
+public class Transaction {
+    @Id
+    @GeneratedValue
+    @Column(name="id", updatable = false, nullable = false)
+    private int id;
+
+    @Column(name="amount", nullable = false)
+    private double amount;
+
+    @Column(name="date", nullable = false)
+    private Date date;
+
+    public Transaction(double amount) {
+        this.amount = amount;
+        this.date = new Date();
+    }
+
+    @OneToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id")
+    @JsonIgnoreProperties({"wallet"})
+    private Account account;
 }
