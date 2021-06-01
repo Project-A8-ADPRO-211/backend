f97191216eebc3b0184547843be0e87f5b9e177a
Naradhipa Bhary <mydhipa@gmail.com>
2021-06-01 22:48:43 +0700

    Emergency Fix - Use H2 for JobRunr

--- src/test/java/com/adpro211/a8/tugaskelompok/TugasKelompokApplicationTests.java
+++ src/test/java/com/adpro211/a8/tugaskelompok/TugasKelompokApplicationTests.java
@@ -1,19 +1,12 @@
 package com.adpro211.a8.tugaskelompok;
 
 import com.adpro211.a8.tugaskelompok.auths.property.AuthProperty;
-import com.adpro211.a8.tugaskelompok.email.config.JobRunrStorageConfig;
 import com.adpro211.a8.tugaskelompok.email.property.EmailProperty;
 import com.adpro211.a8.tugaskelompok.fileupload.storage.StorageProperties;
-import org.jobrunr.jobs.mappers.JobMapper;
-import org.jobrunr.storage.InMemoryStorageProvider;
-import org.jobrunr.storage.StorageProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @SpringBootTest
 @EnableConfigurationProperties({AuthProperty.class, StorageProperties.class, EmailProperty.class})
